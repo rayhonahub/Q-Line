703a1fba190a2b7930a022af18441e340f96e854
farrukht@gmail.com <d661755587@example.com>
2026-06-04 15:41:21 +0500

    commit

--- config/settings.py
+++ config/settings.py
@@ -125,12 +125,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'qline_db',
+        'USER': 'rayhona',
+        'PASSWORD': 'strong_password_here', # пароли худатон
+        'HOST': 'db',  # <--- МАҲЗ ҲАМИН ҶО БОЯД БА ҶОИ 127.0.0.1 КАЛИМАИ 'db' БОШАД!
+        'PORT': '3306',
     }
 }
+
+
 
 
 # Password validation
